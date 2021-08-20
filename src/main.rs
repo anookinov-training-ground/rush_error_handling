31dce92d6d82f7533d6038158adb04a50b7a283a
Alexander Nookinov <anookinov@gmail.com>
2021-08-20 23:03:11 +0700

    add propagating errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::fs::File;
 use std::io::ErrorKind;
+use std::io;
+use std::io::Read;
+use std::fs;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // panic!("crash and burn");
 
     // run with RUST_BACKTRACE=1 cargo run
@@ -45,4 +49,53 @@ fn main() {
 
     let f = File::open("hello.txt").expect("Failed to open hello.txt");
     println!("{:#?}", f);
+
+    let s = read_username_from_file();
+    println!("{:#?}", s);
+
+    let s = read_username_from_file_shortcut();
+    println!("{:#?}", s);
+
+    let s = read_username_from_file_shortcut_chain();
+    println!("{:#?}", s);
+
+    let s = read_username_from_file_to_string();
+    println!("{:#?}", s);
+
+    let f = File::open("hello.txt")?;
+    println!("{:#?}", f);
+    Ok(())
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_shortcut() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file_shortcut_chain() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file_to_string() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
