3f53a7f8f37c9bf4d74e0b7cae5bf22f327002e4
Alexander Nookinov <anookinov@gmail.com>
2021-08-21 10:15:48 +0700

    add how to use panic! vs result

--- src/main.rs
+++ src/main.rs
@@ -62,6 +62,49 @@ fn main() -> Result<(), Box<dyn Error>> {
     let s = read_username_from_file_to_string();
     println!("{:#?}", s);
 
+    use std::net::IpAddr;
+    let home: IpAddr = "127.0.0.1".parse().unwrap();
+    println!("{:#?}", home);
+
+    // loop {
+    //     // --snip--
+
+    //     let guess: i32 = match guess.trim().parse() {
+    //         Ok(num) => num,
+    //         Err(_) => continue,
+    //     };
+
+    //     if guess < 1 || guess > 100 {
+    //         println!("The secret number will be between 1 and 100.");
+    //         continue;
+    //     }
+
+    //     match guess.cmp(&secret_number) {
+    //         // --snip--
+    //     }
+    // }
+
+    pub struct Guess {
+        value: i32,
+    }
+
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}", value);
+            }
+
+            Guess { value }
+        }
+
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+    }
+
+    let guess = Guess::new(11);
+    println!("{:#?}", guess.value());
+
     let f = File::open("hello.txt")?;
     println!("{:#?}", f);
     Ok(())
